ce7662d35233ce249f39cfe6ddb872bff155fe91
parmessan <kolmargar@gmail.com>
2023-02-08 20:16:02 +0200

    Create Lab1.py

---
+++ Lab_1/Lab1.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# -- Sheet --
+
+# # Завдання №2  
+# <br>
+# 
+# ### Mardown tuttorial пройдено. 
+# 
+# ![](https://i1.wp.com/chiwassu.ru/wp-content/uploads/2020/07/1bb1f7613da0448dafd3953c1c510dee_tplv-banciyuan-w650-5.jpg?fit=1024%2C538&ssl=1)
+
+
+# # Завдання №3
+# 
+# # 1. Опрацювання матеріалу
+# * Навчились робити списки, різні стилі тексту
+# * Застосували *markdown*
+# * Порекомендувалиу чаті книгу: [гарна книга](http://loveread.ec/read_book.php?id=76269&p=1)  
+# 
+# <br>
+# <br>
+# 
+# # Heading 1
+# 
+# ## Heading2
+# 
+# якась мудра цитата із Наруто на сірому фоні:
+# >«Истинная честь шиноби не в том, как он живет,   
+# а в том, как он умирает»   
+# >*Джирайя*  
+# 
+# 
+# Можна ще **математикою** займатись: $$y_{sub} =\sqrt(x_{sub})$$  
+# Можна ще і програмуванням:  
+# ```python
+# 
+# import math  
+# 
+# def naruto_voice():
+#     return "даттебае"
+# 
+# ```
+
+
+# # Завдання №4  
+# 
+# <br><br>
+# 
+# #### Here is the sample mean:  
+# $$\overline{y} = \frac{1}{n}\sum_{i = 1}^{n}y_{i}$$
+# 
+# #### Here is the sample variance:
+# $$\sigma^{2} = \frac{\sum_{i = 1}^{n}(y_{i} - \overline{y})^{2}}{n-1}$$
+# 
+# #### Here is the sample standart deviation:
+# $$\sigma = \sqrt{\frac{\sum_{i = 1}^{n}(y_{i} - \overline{y})^{2}}{n-1}}$$
+
+
